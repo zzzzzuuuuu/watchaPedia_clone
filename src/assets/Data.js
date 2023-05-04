8701693149c840b5bae866016603a9872b7a8b34
yhyem <yoonhyemin0120@gmail.com>
2023-05-05 05:09:59 +0900

    setting: 이미지, 데이터 세팅

---
+++ src/assets/Data.js
@@ -0,0 +1,114 @@
+import poster1 from "../assets/image/1.jpg";
+import poster2 from "../assets/image/2.jpg";
+import poster3 from "../assets/image/3.jpg";
+import poster4 from "../assets/image/4.jpg";
+import poster5 from "../assets/image/5.jpg";
+import poster6 from "../assets/image/6.jpg";
+import poster7 from "../assets/image/7.jpg";
+import poster8 from "../assets/image/8.jpg";
+import poster9 from "../assets/image/9.jpg";
+import poster10 from "../assets/image/10.jpg";
+
+export const DATA = [
+  {
+    rank: 1,
+    img: poster1,
+    title: "가디언즈 오브 갤럭시: Volume 3",
+    year: "2023",
+    country: "미국",
+    average: "4.1",
+    percent: "37%",
+    audience: "370000",
+  },
+  {
+    rank: 2,
+    img: poster2,
+    title: "슈퍼 마리오 브라더ㅅ",
+    year: "2023",
+    country: "미국",
+    average: "3.4",
+    percent: "31%",
+    audience: "1070000",
+  },
+  {
+    rank: 3,
+    img: poster3,
+    title: "극장판 짱구는 못말려: 동물소환 닌자 배꼽수비대",
+    year: "2022",
+    country: "일본",
+    average: "3.5",
+    percent: "9.4%",
+    audience: "40000",
+  },
+  {
+    rank: 4,
+    img: poster4,
+    title: "드림",
+    year: "2022",
+    country: "한국",
+    average: "2.8",
+    percent: "5.6%",
+    audience: "750000",
+  },
+  {
+    rank: 5,
+    img: poster5,
+    title: "스즈메의 문단속",
+    year: "2022",
+    country: "일본",
+    average: "3.4",
+    percent: "4%",
+    audience: "5020000",
+  },
+  {
+    rank: 6,
+    img: poster6,
+    title: "분노의 질주: 라이드 오어 다이",
+    year: "2023",
+    country: "미국",
+    average: "",
+    percent: "2.8%",
+    audience: "",
+  },
+  {
+    rank: 7,
+    img: poster7,
+
+    title: "존 윅 4",
+    year: "2023",
+    country: "미국",
+    average: "3.6",
+    percent: "2.2%",
+    audience: "1710000",
+  },
+  {
+    rank: 8,
+    img: poster8,
+    title: "문재인입니다.",
+    year: "2023",
+    country: "한국",
+    average: "",
+    percent: "1.4%",
+    audience: "604",
+  },
+  {
+    rank: 9,
+    img: poster9,
+    title: "더 퍼스트 슬램덩크",
+    year: "2022",
+    country: "일본",
+    average: 4.1,
+    percent: "1.4%",
+    audience: "4060000",
+  },
+  {
+    rank: 10,
+    img: poster10,
+    title: "롱디",
+    year: "2022",
+    country: "한국",
+    average: "",
+    percent: "1.1%",
+    audience: "861",
+  },
+];

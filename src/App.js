bc4374e8b0825d25e59cd7f1aad14b612fa945fd
yhyem <yoonhyemin0120@gmail.com>
2023-05-06 20:50:21 +0900

    feat: 로그인 모달 구현

--- src/App.js
+++ src/App.js
@@ -2,13 +2,12 @@ import "./App.css";
 import RandomClass from "./scss/RandomClass";
 import Button from "./scss/Button";
 import Circle from "./styled-components/Circle";
+import Main from "./page/Main";
 
 function App() {
   return (
     <>
-      <RandomClass>Button</RandomClass>
-      <Button>Button</Button>
-      <Circle />
+      <Main />
     </>
   );
 }
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,5 +1,61 @@
+// ../src/components/Modal.js
+
+import styled from "styled-components";
+
 const Modal = (props) => {
-  return <></>;
+  const { open, close } = props;
+
+  return (
+    <>
+      {open ? (
+        <ModalBackground>
+          <ModalBlock>
+            <TopContent>
+              <CloseButton onClick={close}>&times;</CloseButton>
+            </TopContent>
+            {props.children}
+          </ModalBlock>
+        </ModalBackground>
+      ) : null}
+    </>
+  );
 };
+
+const ModalBackground = styled.div`
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  z-index: 99;
+  background-color: rgba(0, 0, 0, 0.6);
+  text-align: center;
+`;
+
+const ModalBlock = styled.div`
+  width: 350px;
+  height: 600px;
+  margin: auto;
+  border-radius: 5px;
+  background-color: #ffffff;
+  align-items: center;
+`;
+
+const CloseButton = styled.button`
+  display: flex;
+  width: 10px;
+  font-size: 21px;
+  font-weight: 700;
+  text-align: center;
+  color: #696969;
+  background-color: transparent;
+  cursor: pointer;
+  border: white;
+`;
+
+const TopContent = styled.div`
+  position: relative;
+  font-size: 30px;
+`;
 
 export default Modal;

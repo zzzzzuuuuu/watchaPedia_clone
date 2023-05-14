f260cb65c49cda41f20d3e26480283a6a8bc84d6
zzzzzuuuuu <agu0830@naver.com>
2023-05-14 20:46:50 +0900

    rename: 파일명 수정

--- src/page/Main.js
+++ src/page/Main.js
@@ -1,75 +1,75 @@
-import { useState } from "react";
-import { styled } from "styled-components";
-import BoxOffice from "../components/BoxOffice";
-import Data from "../page/Data.json";
-import Logo from "../assets/image/icon_logo.png";
-import Modal from "../components/Modal";
-import { BiSearch } from "react-icons/bi";
+// import { useState } from "react";
+// import { styled } from "styled-components";
+import Movies from "../components/Movies";
+import {DATA} from "../assets/Data";
+// import Logo from "../assets/image/icon_logo.png";
+// import Modal from "../components/Modal";
+// import { BiSearch } from "react-icons/bi";
+// import MovieInfos from "../components/MovieInfos";
+// import {Route, Routes} from "react-router-dom";
 
 const Main = () => {
-  const [isButton, setIsButton] = useState(false);
+  // const [isButton, setIsButton] = useState(false);
   return (
     <>
-      <HeaderBar>
-        <Img src={Logo}></Img>
-        <Category style={{ color: "black" }}>영화</Category>
-        <Category>TV</Category>
-        <Category>책</Category>
-        <Category>웹툰</Category>
-        <Gap></Gap>
-        {/* <SearchIcon></SearchIcon>{" "}
-        돋보기 나중에 다시 제대로 생각하기 ^^!! */}
-        <BiSearch
-          style={{
-            marginTop: "10px",
-            padding: "7px 6px 7px 6px",
-            width: "20px",
-            height: "20px",
-            background: "#f5f5f7",
-            borderRadius: "5px",
-            border: "1px solid #f5f5f7",
-          }}
-        />
-        <Search placeholder=" 콘텐츠, 인물, 유저를 검색해보세요."></Search>
-        <LoginButton
-          onClick={() => {
-            setIsButton(true);
-          }}
-        >
-          로그인
-        </LoginButton>
-        <SignupButton>회원가입</SignupButton>
-      </HeaderBar>
-      <BoxOffice data={Data} />
-      {isButton ? <Modal /> : null}
+      {/*<HeaderBar>*/}
+      {/*  <Img src={Logo}></Img>*/}
+      {/*  <Category style={{ color: "black" }}>영화</Category>*/}
+      {/*  <Category>TV</Category>*/}
+      {/*  <Category>책</Category>*/}
+      {/*  <Category>웹툰</Category>*/}
+      {/*  <Gap></Gap>*/}
+      {/*  <BiSearch*/}
+      {/*    style={{*/}
+      {/*      marginTop: "10px",*/}
+      {/*      padding: "7px 6px 7px 6px",*/}
+      {/*      width: "20px",*/}
+      {/*      height: "20px",*/}
+      {/*      background: "#f5f5f7",*/}
+      {/*      borderRadius: "5px",*/}
+      {/*      border: "1px solid #f5f5f7",*/}
+      {/*    }}*/}
+      {/*  />*/}
+      {/*  <Search placeholder=" 콘텐츠, 인물, 유저를 검색해보세요."></Search>*/}
+      {/*  <LoginButton*/}
+      {/*    onClick={() => {*/}
+      {/*      setIsButton(true);*/}
+      {/*    }}*/}
+      {/*  >*/}
+      {/*    로그인*/}
+      {/*  </LoginButton>*/}
+      {/*  <SignupButton>회원가입</SignupButton>*/}
+      {/*</HeaderBar>*/}
+      <Movies data={DATA} />
+      {/*{isButton ? <Modal /> : null}*/}
     </>
   );
 };
 
 export default Main;
 
-const HeaderBar = styled.div`
-  display: flex;
-  margin: 0 40px;
-  padding-bottom: 15px;
-  align-items: center;
-  border-bottom: 1px solid #ebebeb;
-`;
-const Img = styled.img`
-  width: 150px;
-  margin: 10px 15px 0 15px;
-`;
-
-const Category = styled.span`
-  margin: 25px 10px 13px 10px;
-  color: #7e7e7e;
-  /* margin-top: 25px; */
-  font-weight: 600;
-`;
-
-const Gap = styled.span`
-  width: 32em;
-`;
+// const HeaderBar = styled.div`
+//   display: flex;
+//   margin: 0 40px;
+//   padding-bottom: 15px;
+//   align-items: center;
+//   border-bottom: 1px solid #ebebeb;
+// `;
+// const Img = styled.img`
+//   width: 150px;
+//   margin: 10px 15px 0 15px;
+// `;
+//
+// const Category = styled.span`
+//   margin: 25px 10px 13px 10px;
+//   color: #7e7e7e;
+//   /* margin-top: 25px; */
+//   font-weight: 600;
+// `;
+//
+// const Gap = styled.span`
+//   width: 32em;
+// `;
 
 // const SearchIcon = styled.img`
 //   margin-top: 10px;
@@ -84,38 +84,38 @@ const Gap = styled.span`
 //   border: 1px solid #f5f5f7;
 // `;
 
-const Search = styled.input`
-  width: 240px;
-  height: 34px;
-  margin-left: -5px;
-  margin-top: 10px;
-  background-color: #f5f5f7;
-  border-radius: 5px;
-  border: none;
-  font-size: 15px;
-  font-weight: 600;
-  color: #74747a;
-`;
-
-const LoginButton = styled.button`
-  margin-top: 15px;
-  width: 80px;
-  height: 35px;
-  border: none;
-  color: #74747a;
-  background: white;
-  font-size: 15px;
-  font-weight: 600;
-`;
-
-const SignupButton = styled.button`
-  margin-top: 15px;
-  width: 83px;
-  height: 35px;
-  border: 1px solid #cfcfcf;
-  color: black;
-  font-size: 15px;
-  font-weight: 600;
-  background: white;
-  border-radius: 5px;
-`;
+// const Search = styled.input`
+//   width: 240px;
+//   height: 34px;
+//   margin-left: -5px;
+//   margin-top: 10px;
+//   background-color: #f5f5f7;
+//   border-radius: 5px;
+//   border: none;
+//   font-size: 15px;
+//   font-weight: 600;
+//   color: #74747a;
+// `;
+//
+// const LoginButton = styled.button`
+//   margin-top: 15px;
+//   width: 80px;
+//   height: 35px;
+//   border: none;
+//   color: #74747a;
+//   background: white;
+//   font-size: 15px;
+//   font-weight: 600;
+// `;
+//
+// const SignupButton = styled.button`
+//   margin-top: 15px;
+//   width: 83px;
+//   height: 35px;
+//   border: 1px solid #cfcfcf;
+//   color: black;
+//   font-size: 15px;
+//   font-weight: 600;
+//   background: white;
+//   border-radius: 5px;
+// `;
